cc925ad5968f2f9b44ed21912e6bc1113f375d6f
Develop-KIM <kimdonghwan913@gmail.com>
2025-11-27 09:00:56 +0900

    chore: nuxt 모듈, 의존성 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,26 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-11-27',
   devtools: { enabled: true },
   
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',  
+    '@nuxt/icon',
+  ],
+
+  colorMode: {
+    classSuffix: '',
+    preference: 'system', 
+    fallback: 'light',
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL || 'http://localhost:3163'
+    }
+  },
+    
+  css: ['~/assets/css/main.css'],
 })
